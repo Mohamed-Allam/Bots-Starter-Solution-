cde7bde6d695cb0cde6395596a286c9d5bdd51fe
Mohamed-Allam <Eng_MohamedAllam@hotmail.com>
2018-04-17 18:26:42 +0300

    Thank You

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ const bot = module.exports = new builder.UniversalBot(connector,
     });
 
     bot.recognizer(new builder.RegExpRecognizer( "tenantIntent", /^(tenant|mosta2ger)/i ));
-    bot.recognizer(new builder.RegExpRecognizer( "maintenanceIntent", /^(Maintainance)/i ));
+    bot.recognizer(new builder.RegExpRecognizer( "maintenanceIntent", /^(Maintenance)/i ));
 
 // Send welcome when conversation with bot is started, by initiating the root dialog
 bot.on('conversationUpdate', function (message) {
@@ -125,29 +125,79 @@ bot.dialog('tenant', [
 
     }, (session,results,next) => {
 
-     builder.Prompts.choice(session, "How Can I Help You Today?", "Maintainance|Termination Letter", { listStyle: builder.ListStyle.button });
+     builder.Prompts.choice(session, "How Can I Help You Today?", "Maintenance|Termination Letter", { listStyle: builder.ListStyle.button });
 
-    }, (session,results,next) => {
+    }
+    , (session,results,next) => {
 
-        session.send(" Your choice was " + results.response.entity)
-   
+       if(results.response.entity == "Maintenance")
+       session.beginDialog("maintenanceDialog");
+       else if (results.response.entity == "terminationDialog")
+       session.beginDialog("terminationDialog");
+       session.endDialogWithResult({parm:"XXX"});
        }
 ]).triggerAction({ matches: 'tenantIntent' });
 
 
-bot.dialog('maintainanceDialog', [
-    (session, args, next) => {
-       session.send("You Choose " + args.results)
-    },
-    (session, results, next) => {
+bot.dialog('maintenanceDialog', [
+    (session) => {
+        var cards = getCardsAttachments();
+
+        // create reply with Carousel AttachmentLayout
+          var reply = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments(cards);
+    
+        session.send(reply);
+
+            function getCardsAttachments(session) {
+                return [
+                    new builder.HeroCard(session)
+                        .title('Azure Storage')
+                        .subtitle('Offload the heavy lifting of data center management')
+                        .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+                        .images([
+                            builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/aspnet/aspnet/overview/developing-apps-with-windows-azure/building-real-world-cloud-apps-with-windows-azure/data-storage-options/_static/image5.png')
+                        ])
+                        .buttons([
+                            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+                        ]),
+            
+                    new builder.ThumbnailCard(session)
+                        .title('DocumentDB')
+                        .subtitle('Blazing fast, planet-scale NoSQL')
+                        .images([
+                            builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+                        ])
+                        .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+                        .buttons([
+                            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+                        ]),
+            
+                    new builder.HeroCard(session)
+                        .title('Azure Functions')
+                        .subtitle('Process events with a serverless code architecture')
+                        .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+                        .images([
+                            builder.CardImage.create(session, 'https://msdnshared.blob.core.windows.net/media/2016/09/fsharp-functions2.png')
+                        ])
+                        .buttons([
+                            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+                        ]),
+            
+                    new builder.ThumbnailCard(session)
+                        .title('Cognitive Services')
+                        .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+                        .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+                        .images([
+                            builder.CardImage.create(session, 'https://msdnshared.blob.core.windows.net/media/2017/03/Azure-Cognitive-Services-e1489079006258.png')
+                        ])
+                        .buttons([
+                            builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+                        ])
+                ];
+            }
        
-    }, (session,results,next) => {
-
-        
-    }, (session,results,next) => {
-
- 
-
     }
 ]).triggerAction({ matches: 'maintenanceIntent' });
 
