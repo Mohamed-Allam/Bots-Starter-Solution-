01d8c2b0cc58c8e86ef3728e516eca8ffb30ad89
Mohamed-Allam <Eng_MohamedAllam@hotmail.com>
2018-04-18 22:49:37 +0300

    clean up the comments

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 'use strict';
 const builder = require('botbuilder');
 var data = require("./Demodata.js");
+var Promise = require('bluebird');
+var request = require('request-promise').defaults({ encoding: null });
+var fs = require("fs");
 
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -134,7 +137,7 @@ bot.dialog('tenant', [
         setTimeout(function () {
             next();
         }, 3000);
-       
+
 
     }, (session, results, next) => {
 
@@ -152,53 +155,12 @@ bot.dialog('tenant', [
 ]).triggerAction({ matches: 'tenantIntent' });
 
 
+
+
 bot.dialog('maintenanceDialog', [
     (session) => {
 
         // "https://docs.microsoft.com/en-us/aspnet/aspnet/overview/developing-apps-with-windows-azure/building-real-world-cloud-apps-with-windows-azure/data-storage-options/_static/image5.png"
-        var choices = [
-            {
-                title: "Domestic Drianage System", subtitle: "Can you See Grease, Tree roots or Silted Drains ?",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Driange System"
-            },
-
-            {
-                title: "Elevator System Problems", subtitle: "Power failure | Lights Off | Noisy bearings",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Elevator Problem"
-            },
-
-            {
-                title: "irrigation System", subtitle: "No water coming | Zones Stopping | Sprinkles",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Irrigation"
-            },
-
-            {
-                title: "Exhaust Fan", subtitle: "Motor Noise | Rattling | Power Failure | Moisture",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Exhaust Fan"
-            },
-
-            {
-                title: "Water Heater Leakage", subtitle: "Can you see water Leaking from the Heater ?",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Leakage"
-            },
-
-            {
-                title: "Water Heater Break Down", subtitle: "No Hot Water | Not Enough Water | Too Cold  ",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Heater"
-            },
-
-            {
-                title: "Water Supply Cut", subtitle: "No water is coming from the water taps ?",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Cut"
-            },
-
-            {
-                title: "LPG System", subtitle: "Offload the heavy lifting of LPG Systems",
-                imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "LPG"
-            }
-        ];
-
-
         var cards = [];
         var stringChoices = []
 
@@ -234,91 +196,70 @@ bot.dialog('maintenanceDialog', [
     , (session, results) => {
         session.dialogData.choice = results.response.entity;
         builder.Prompts.text(session, "Please Describe Your Problem");
-    }, (session, results) => {
-        session.send("PLease take a shot and send the picture to Me ");
+    }, (session, results,next) => {
+        
+        builder.Prompts.attachment(session,"PLease take a shot and send the picture to Me ");
+        
+    },
+    (session,results) => {
+
+          // Request file with Authentication Header
+          var requestWithToken = function (url) {
+            return obtainToken().then(function (token) {
+                return request({
+                    url: url,
+                    headers: {
+                        'Authorization': 'Bearer ' + token,
+                        'Content-Type': 'application/octet-stream'
+                    }
+                });
+            });
+        };
+
+        // Promise for obtaining JWT Token (requested once)
+        var obtainToken = Promise.promisify(connector.getAccessToken.bind(connector));
+
+        var checkRequiresToken = function (message) {
+            return message.source === 'skype' || message.source === 'msteams';
+        };
+
+        var msg = session.message;
+        if (msg.attachments.length) {
+
+            // Message with attachment, proceed to download it.
+            // Skype & MS Teams attachment URLs are secured by a JwtToken, so we need to pass the token from our bot.
+            var attachment = msg.attachments[0];
+            var fileDownload = checkRequiresToken(msg)
+                ? requestWithToken(attachment.contentUrl)
+                : request(attachment.contentUrl);
+
+                request(attachment.contentUrl).pipe(fs.createWriteStream('metro.png'));
+
+            fileDownload.then(
+                function (response) {
+
+                    // Send reply with attachment type & size
+                    var reply = new builder.Message(session)
+                        .text('Attachment of %s type and size of %s bytes received.', attachment.contentType, response.length);
+                    session.send(reply);
+
+                }).catch(function (err) {
+                    console.log('Error downloading attachment:', { statusCode: err.statusCode, message: err.response.statusMessage });
+                });
+
+        } else {
+
+            // No attachments were sent
+            var reply = new builder.Message(session)
+                .text('Hi there! This sample is intented to show how can I receive attachments but no attachment was sent to me. Please try again sending a new message with an attachment.');
+            session.send(reply);
+        }
+
+      
+
     }
 ]).triggerAction({ matches: 'maintenanceIntent' });
 
-bot.dialog('getAge', [
-    (session, args, next) => {
-        let name = session.dialogData.name = 'User';
-
-        if (args) {
-            // store reprompt flag
-            session.dialogData.isReprompt = args.isReprompt;
-
-            // retrieve name
-            name = session.dialogData.name = args.name;
-        }
-
-        // prompt user
-        builder.Prompts.number(session, `How old are you, ${name}?`);
-    },
-    (session, results, next) => {
-        const age = results.response;
-
-        // Basic validation - did we get a response?
-        if (!age || age < 13 || age > 90) {
-            // Bad response. Logic for single re-prompt
-            if (session.dialogData.isReprompt) {
-                // Re-prompt ocurred
-                // Send back empty string
-                session.endDialogWithResult({ response: '' });
-            } else {
-                // Set the flag
-                session.dialogData.didReprompt = true;
-                session.send(`Sorry, that doesn't look right.`);
-                // Call replaceDialog to start the dialog over
-                // This will replace the active dialog on the stack
-                session.replaceDialog('getAge',
-                    { name: session.dialogData.name, isReprompt: true });
-            }
-        } else {
-            // Valid age received
-            // Return control to calling dialog
-            // Pass the age in the response property of results
-            session.endDialogWithResult({ response: age });
-        }
-    }
-]);
-
-bot.dialog('AddNumber', [
-    (session, args, next) => {
-        let message = null;
-        if (!session.privateConversationData.runningTotal) {
-            message = `Give me the first number.`;
-            session.privateConversationData.runningTotal = 0;
-        } else {
-            message = `Give me the next number, or say **total** to display the total.`;
-        }
-        builder.Prompts.number(session, message, { maxRetries: 3 });
-    },
-    (session, results, next) => {
-        if (results.response) {
-            session.privateConversationData.runningTotal += results.response;
-            session.replaceDialog('AddNumber');
-        } else {
-            session.endConversation(`Sorry, I don't understand. Let's start over.`);
-        }
-    },
-])
-    .triggerAction({ matches: /^add$/i })
-    .cancelAction('CancelAddNumber', 'Operation cancelled', {
-        matches: /^cancel$/,
-        onSelectAction: (session, args, next) => {
-            session.endConversation(`Operation cancelled.`);
-            next();
-        },
-        confirmPrompt: `Are you sure you wish to cancel?`
-    })
-    .beginDialogAction('Total', 'Total', { matches: /^total$/ })
-    .beginDialogAction('HelpAddNumber', 'Help', { matches: /^help$/, dialogArgs: { action: 'AddNumber' } });
-
-bot.dialog('Total', [
-    (session, results, next) => {
-        session.endConversation(`The total is ${session.privateConversationData.runningTotal}`);
-    },
-]);
 
 bot.dialog('Help', [
     (session, args, next) => {
@@ -332,6 +273,50 @@ bot.dialog('Help', [
         session.beginDialog(args.action, args);
     }
 });
+
+
+var choices = [
+    {
+        title: "Domestic Drianage System", subtitle: "Can you See Grease, Tree roots or Silted Drains ?",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Driange System"
+    },
+
+    {
+        title: "Elevator System Problems", subtitle: "Power failure | Lights Off | Noisy bearings",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Elevator Problem"
+    },
+
+    {
+        title: "irrigation System", subtitle: "No water coming | Zones Stopping | Sprinkles",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Irrigation"
+    },
+
+    {
+        title: "Exhaust Fan", subtitle: "Motor Noise | Rattling | Power Failure | Moisture",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Exhaust Fan"
+    },
+
+    {
+        title: "Water Heater Leakage", subtitle: "Can you see water Leaking from the Heater ?",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Leakage"
+    },
+
+    {
+        title: "Water Heater Break Down", subtitle: "No Hot Water | Not Enough Water | Too Cold  ",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Heater"
+    },
+
+    {
+        title: "Water Supply Cut", subtitle: "No water is coming from the water taps ?",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "Water Cut"
+    },
+
+    {
+        title: "LPG System", subtitle: "Offload the heavy lifting of LPG Systems",
+        imgUrl: "https://d30y9cdsu7xlg0.cloudfront.net/png/20556-200.png", msg: "LPG"
+    }
+];
+
 
 var createRichMessage = function (img, msg, title) {
 
@@ -388,73 +373,5 @@ var createRichMessage = function (img, msg, title) {
 
     return card;
 }
-
-// The initial Water fall before using the intents 
-
-// const bot = module.exports = new builder.UniversalBot(connector, [
-//     // this section becomes the root dialog
-//     // If a conversation hasn't been started, and the message
-//     // sent by the user doesn't match a pattern, the
-//     // conversation will start here
-//     (session, args, next) => {
-
-//         // This Line Keeps the Welcome card from reaching the top of the screen
-//         session.send(" Hi from the root Dialog \n \n ");
-
-
-//         var welcomeCard = new builder.HeroCard(session)
-//         .title('welcome_title')
-//         .subtitle('welcome_subtitle')
-//         .images([
-//             new builder.CardImage(session)
-//                 .url(links.logo)
-//                 .alt('contoso_flowers')
-//         ])
-//         .buttons([
-//             builder.CardAction.imBack(session, session.gettext(MainOptions.Shop), MainOptions.Shop),
-//             builder.CardAction.imBack(session, session.gettext(MainOptions.Support), MainOptions.Support)
-//         ]);
-
-
-//     session.send(new builder.Message(session)
-//         .addAttachment(welcomeCard));
-
-//         // Launch the getName dialog using beginDialog
-//         // When beginDialog completes, control will be passed
-//         // to the next function in the waterfall
-
-//           session.beginDialog('tenant');
-//     },
-//     (session, results, next) => {
-//         // executed when getName dialog completes
-//         // results parameter contains the object passed into endDialogWithResults
-
-//         // check for a response
-//         if (results.response) {
-//             const name = session.privateConversationData.name = results.response;
-
-//             // When calling another dialog, you can pass arguments in the second parameter
-//             session.beginDialog('getAge', { name: name });
-//         } else {
-//             // no valid response received - End the conversation
-//             session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
-//         }
-//     },
-//     (session, results, next) => {
-//         // executed when getAge dialog completes
-//         // results parameter contains the object passed into endDialogWithResults
-
-//         // check for a response
-//         if (results.response) {
-//             const age = session.privateConversationData.age = results.response;
-//             const name = session.privateConversationData.name;
-
-//             session.endConversation(`Hello ${name}. You are ${age}`);
-//         } else {
-//             // no valid response received - End the conversation
-//             session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
-//         }
-//     },
-// ]);
 
 
